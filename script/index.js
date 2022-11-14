718da32aa3f9b4ba5ddf9bb82b1bfc3288aa56b9
Andrey Klabukov <andr1k002022@gmail.com>
2022-11-14 13:51:18 +0400

    fix: fixed second iteration review

--- script/index.js
+++ script/index.js
@@ -1,46 +1,33 @@
 let popup = document.querySelector('.popup');
-let profileInfo = popup.querySelector('.popup_user_info');
-let profileUsename = popup.querySelector('.popup_user_name');
 let profileClose = popup.querySelector('.popup__close');
 let profileEdit = document.querySelector('.profile__edit');
 let userName = document.querySelector('.profile__user-name');
 let userInfo = document.querySelector('.profile__user-info');
-
-function openPopup(){
-  popup.classList.toggle('popup_opened');
-}
-
-profileClose.addEventListener('click', openPopup);
-
-profileEdit.addEventListener('click', openPopup);
-
-// Находим форму в DOM
 let formElement = document.querySelector('.popup__content');
-// Находим поля формы в DOM
 let nameInput = popup.querySelector('.popup__info_user_name');
 let jobInput = popup.querySelector('.popup__info_user_info');
 
-nameInput.value = userName.textContent;
-jobInput.value = userInfo.textContent;
+function openPopup(){
+  popup.classList.toggle('popup_opened');
+  nameInput.value = userName.textContent;
+  jobInput.value = userInfo.textContent;
+}
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+function closePopup(){
+  popup.classList.remove('popup_opened');
+}
+
 function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
+    evt.preventDefault();
 
-    // Получите значение полей jobInput и nameInput из свойства value
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-    
-    // Вставьте новые значения с помощью textContent
     userName.textContent = nameInput.value;
     userInfo.textContent = jobInput.value;
     
-    openPopup();
+    closePopup();
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
+profileClose.addEventListener('click', closePopup);
+
+profileEdit.addEventListener('click', openPopup);
+
 formElement.addEventListener('submit', formSubmitHandler); 
