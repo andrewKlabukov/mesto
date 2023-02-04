b69392f67d7cc97ad72c0a0d14c0501e363a0fb4
Andrey Klabukov <andr1k002022@gmail.com>
2023-02-04 19:49:51 +0400

    fix: bugs in js

---
+++ script/validate.js
@@ -0,0 +1,12 @@
+const formSettings = ({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+});
+
+function enableValidation (){
+
+}
